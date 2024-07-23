ee7e7e2ba70d4c70914df5bd6112fbb276a03ec7
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-07-23 22:22:49 +0500

    adding photos

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+let elemContainer = document.querySelector("#elem-container");
+let fixImage = document.querySelector("#fixed-image");
+elemContainer.addEventListener("mouseenter" , () => {
+    // alert("nice")
+    fixImage.style.display = "block"
+});
+elemContainer.addEventListener("mouseleave" , () => {
+    // alert("nice")
+    fixImage.style.display = "none"
+});
+let elem1 = document.querySelector("#elem1")
+elem1.addEventListener("mouseenter" , () => {
+    let image = elem1.getAttribute("data-image")
+    fixImage.style.backgroundImage = `url(${image})`
+})
